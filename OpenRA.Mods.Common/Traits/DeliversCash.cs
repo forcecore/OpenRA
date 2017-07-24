1828c6dd6378ee864309aa588ffd55f964c32302
Forcecore <ssanzing@gmail.com>
2017-07-24 10:38:37 -0500

    Sound and Animation support for DonateCash

--- OpenRA.Mods.Common/Traits/DeliversCash.cs
+++ OpenRA.Mods.Common/Traits/DeliversCash.cs
@@ -9,21 +9,17 @@
  */
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using OpenRA.Mods.Common.Activities;
 using OpenRA.Mods.Common.Orders;
 using OpenRA.Traits;
 
-/*
-// Modifications for Mod.yupgi_alert:
-// Add sound effect parameter and INotifyCashTransfer.
-*/
-
 namespace OpenRA.Mods.Common.Traits
 {
 	[Desc("Donate money to actors with the `AcceptsDeliveredCash` trait.")]
-	public class DeliversCashInfo : ITraitInfo
+	class DeliversCashInfo : ITraitInfo
 	{
 		[Desc("The amount of cash the owner receives.")]
 		public readonly int Payload = 500;
@@ -42,7 +38,7 @@ namespace OpenRA.Mods.Common.Traits
 		public object Create(ActorInitializer init) { return new DeliversCash(this); }
 	}
 
-	public class DeliversCash : IIssueOrder, IResolveOrder, IOrderVoice, INotifyCashTransfer
+	class DeliversCash : IIssueOrder, IResolveOrder, IOrderVoice, INotifyCashTransfer
 	{
 		readonly DeliversCashInfo info;
 
